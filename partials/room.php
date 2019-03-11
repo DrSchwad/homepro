1e6606d0f56c446af5e2f19e9b3a9963b9ed08c7
DrSwad <drswad2013@gmail.com>
2019-03-11 22:09:57 +0600

    Fixed links in room page

--- partials/room.php
+++ partials/room.php
@@ -1,35 +1,35 @@
 <div id="room" class="col-md-9 mid">
     <img src="img/imgmin/room/background-hexagon.png" />
     <div class="hex-wrapper">
-        <div class="hex1 hex-container" onclick="window.location.href='/catalogue/Kitchen'">
+        <div class="hex1 hex-container" onclick="window.location.href='/catalogue.php?view=Kitchen'">
             <div class="text"><span>kitchen</span></div>
             <img src="img/imgmin/room/kitchen.png" />
         </div>
-        <div class="hex2 hex-container" onclick="window.location.href='/catalogue/Dining'">
+        <div class="hex2 hex-container" onclick="window.location.href='/catalogue.php?view=Dining'">
             <div class="text"><span>Dining</span></div>
             <img src="img/imgmin/room/dining.png" />
         </div>
-        <div class="hex3 hex-container" onclick="window.location.href='/catalogue/Bedroom'">
+        <div class="hex3 hex-container" onclick="window.location.href='/catalogue.php?view=Bedroom'">
             <div class="text"><span>Bedroom</span></div>
             <img src="img/imgmin/room/bedroom.png" />
         </div>
-        <div class="hex4 hex-container" onclick="window.location.href='/catalogue/Sofa Set'">
+        <div class="hex4 hex-container" onclick="window.location.href='/catalogue.php?view=Sofa_Set'">
             <div class="text"><span>Sofa</span></div>
             <img src="img/imgmin/room/sofa.png" />
         </div>
-        <div class="hex5 hex-container" onclick="window.location.href='/catalogue/Home Decor'">
+        <div class="hex5 hex-container" onclick="window.location.href='/catalogue.php?view=Home_Decor'">
             <div class="text"><span>Home decor</span></div>
             <img src="img/imgmin/room/home-decor.png" />
         </div>
-        <div class="hex6 hex-container" onclick="window.location.href='/catalogue/Office Furniture'">
+        <div class="hex6 hex-container" onclick="window.location.href='/catalogue.php?view=Office_Furniture'">
             <div class="text"><span>Office furniture</span></div>
             <img src="img/imgmin/room/office.png" />
         </div>
-        <div class="hex7 hex-container" onclick="window.location.href='/catalogue/Painting'">
+        <div class="hex7 hex-container" onclick="window.location.href='/catalogue.php?view=Painting'">
             <div class="text"><span>Painting</span></div>
             <img src="img/imgmin/room/painting.png" />
         </div>
-        <div class="hex8 hex-container" onclick="window.location.href='/catalogue/Hardware'">
+        <div class="hex8 hex-container" onclick="window.location.href='/catalogue.php?view=Hardware'">
             <div class="text"><span>Hardware</span></div>
             <img src="img/imgmin/room/hardware.png" />
         </div>
